659e4a7e83894d42ae5485036d7bf92959e1c791
littleStar <1582725691@qq.com>
2023-02-15 12:05:31 +0800

    更新第二章

--- d2l/utils.py
+++ d2l/utils.py
@@ -1,7 +1,64 @@
 import matplotlib.pyplot as plt
 from PIL import Image
 import torch
+import torch, torchvision
+from torch.utils.data import DataLoader
 
+def evaluate_accuracy(data_iter, net):
+    acc_sum, n = 0.0, 0
+    for x, y in data_iter:
+        acc_sum += (net(x).argmax(axis=1)==y).type(torch.float32).sum().item()
+        n += y.shape[0]
+    return acc_sum / n
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size,
+             params=None, lr=None, optimizer=None):
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for x, y in train_iter:
+            if optimizer is not None:
+                optimizer.zero_grad()
+            elif params is not None and params[0].grad is not None:
+                for param in params:
+                    param.grad.data.zero_()
+                    
+            y_hat = net(x)
+            l = loss(y_hat, y).sum()
+            l.backward()
+            
+            if optimizer is None:
+                SGD(params, lr, batch_size)
+            else:
+                optimizer.step()
+            
+            train_l_sum += l.item()
+            train_acc_sum += (y_hat.argmax(axis=1)==y).type(torch.float32).sum().item()
+            n += y.shape[0]
+            
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+             % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+def load_fashion_mnist_with_batch(transform, batch_size):
+    train_set = torchvision.datasets.FashionMNIST(
+        root='./../data/FashionMNIST',
+        train=True,
+        download=False,
+        transform=transform
+    )
+
+    test_set = torchvision.datasets.FashionMNIST(
+        root='./../data/FashionMNIST',
+        train=False,
+        download=False,
+        transform=transform
+    )
+    
+    train_DataLoader = DataLoader(train_set, batch_size=batch_size, 
+                              shuffle=True,num_workers=0)
+    test_DataLoader = DataLoader(test_set, batch_size=batch_size, 
+                              shuffle=True,num_workers=0)
+    return train_DataLoader, test_DataLoader
 
 def get_fashion_mnist_labels(labels):
     """
@@ -61,3 +118,5 @@ def show_fashion_mnist(images, labels, transform):
 def SGD(params, lr, batch_size):
     for param in params:
         param.data -= lr * param.grad / batch_size
+
+        
